e46f79647e60f63bc1d4ce3cef87743014d7d0d9
JoshNeely <Janeely@uncg.edu>
2022-11-15 03:33:13 -0500

    Added New color themes to html pages and added hover feature

--- RealEstate/src/main/java/net/codejava/HouseManagerApplication.java
+++ RealEstate/src/main/java/net/codejava/HouseManagerApplication.java
@@ -10,5 +10,4 @@ public class HouseManagerApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(HouseManagerApplication.class, args);
 	}
-
 }
