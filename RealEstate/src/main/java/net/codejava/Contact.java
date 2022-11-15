0da61837a902d47d41f3d2335127716cd9e4bbdb
XFreeman01 <xjfreema@uncg.edu>
2022-11-15 06:56:32 -0500

    commit

---
+++ RealEstate/src/main/java/net/codejava/Contact.java
@@ -0,0 +1,36 @@
+package net.codejava;
+
+public class Contact {
+
+	private String firstName;
+	private String lastName;
+	private String landlordName;
+	private String number;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getLandlordName() {
+		return landlordName;
+	}
+	public void setLandlordName(String landlordName) {
+		this.landlordName = landlordName;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	} 
+
+
+	
+}
